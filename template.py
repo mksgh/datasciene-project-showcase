119478fe53b279df874f5765a67e8a0518837c36
manik singh <109017266+mksgh@users.noreply.github.com>
2025-03-06 01:35:06 +0000

    Added new scipts and yaml files
    -- updated source configration
    -- updated entity

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
-project_name = "ds-project"
+project_name = "dsproject"
 
 list_of_files = [
     ".gthub/workflows/.gitkeep",
