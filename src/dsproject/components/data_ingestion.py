27575f67db5ab10ff6a04fd1ccceeda5c7dc1051
manik singh <109017266+mksgh@users.noreply.github.com>
2025-03-10 01:07:13 +0000

    Updated data_ingestion.py & added data_transformation.py file

--- src/dsproject/components/data_ingestion.py
+++ src/dsproject/components/data_ingestion.py
@@ -1,8 +1,8 @@
 import os
 import urllib.request as request
-from src.datascience import logger
+from src.dsproject import logger
 import zipfile
-from src.datascience.entity.config_entity import DataIngestionConfig
+from src.dsproject.entity.config_entity import DataIngestionConfig
 
 
 ## component-Data Ingestion
@@ -13,6 +13,8 @@ class DataIngestion:
     
     # Downloading the zip file
     def download_file(self):
+        """Download the zip file from the source
+        """
         if not os.path.exists(self.config.local_data_file):
             filename, headers = request.urlretrieve(
                 url = self.config.source_URL,
@@ -20,15 +22,16 @@ class DataIngestion:
             )
             logger.info(f"{filename} download! with following info: \n{headers}")
         else:
-            logger.info(f"File already exists")
+            logger.info(f"'{self.config.local_data_file}' file already exists")
 
     def extract_zip_file(self):
-        """
+        """Extracts the zip file into the data directory
+
         zip_file_path: str
-        Extracts the zip file into the data directory
-        Function returns None
+        Returns: None
         """
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
+        
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
---
+++ src/dsproject/components/data_transformation.py
@@ -0,0 +1,28 @@
+import os
+from src.dsproject import logger
+from sklearn.model_selection import train_test_split
+from src.dsproject.entity.config_entity import DataTransformationConfig
+import pandas as pd
+
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+
+    def train_test_splitting(self):
+        """Split data into train and test and saving them to csv file"""
+
+        data = pd.read_csv(self.config.data_path)
+
+        # Split the data into training and test sets.
+        train, test = train_test_split(data, test_size=0.20)
+
+        train.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)
+        test.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)
+
+        logger.info("Splitted the data into training and test sets")
+        logger.info(f"Shape of train data {train.shape}")
+        logger.info(f"Shape of train data {train.shape}")
+
+        print(f"Shape of train data {train.shape}")
+        print(f"Shape of train data {train.shape}")
