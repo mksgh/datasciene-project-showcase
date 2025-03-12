ae7e8ff30c37c1c5c32b6566c17011b0448eb04d
manik singh <109017266+mksgh@users.noreply.github.com>
2025-03-12 02:15:56 +0000

    updated data ingestion

--- src/dsproject/pipeline/data_ingestion_pipeline.py
+++ src/dsproject/pipeline/data_ingestion_pipeline.py
@@ -1,6 +1,5 @@
 from src.dsproject.config.configuration import ConfigurationManager
 from src.dsproject.components.data_ingestion import DataIngestion
-from src.dsproject import logger
 
 
 STAGE_NAME="Data Ingestion Stage"
@@ -15,14 +14,3 @@ class DataIngestionTrainingPipeline:
         data_ingestion=DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
-
-
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        data_ingestion_obj = DataIngestionTrainingPipeline()
-        data_ingestion_obj.initiate_data_ingestion()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-    except Exception as e:
-        logger.exception(e)
-        raise e
